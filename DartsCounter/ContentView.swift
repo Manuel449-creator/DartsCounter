e57391f19189954ad28bbcfe385597bef151c6fb
Manuel <manuelwagner23@outlook.de>
2025-01-28 09:55:36 +0100

    Initial Commit

---
+++ DartsCounter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DartsCounter
+//
+//  Created by Manuel Wagner on 28.01.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DartsCounter/DartsCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DartsCounterApp.swift
+//  DartsCounter
+//
+//  Created by Manuel Wagner on 28.01.25.
+//
+
+import SwiftUI
+
+@main
+struct DartsCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
